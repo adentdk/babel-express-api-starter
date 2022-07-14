1385fc1be7af8ba7c391a7a1e0e5d95360febb0a
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2022-07-14 13:24:07 +0700

    :sparkles: add crypto utils
    clear

---
+++ src/utils/crypto.js
@@ -0,0 +1,53 @@
+import * as crypto from 'crypto';
+
+const algorithm = 'aes-256-gcm';
+const iterations = 2145;
+const keylen = 32;
+const digest = 'sha512';
+const salt = crypto.randomBytes(64);
+
+export const encrypt = (data, secretKey) => {
+  const inputEncoding = 'utf8';
+  const outputEncoding = 'base64';
+
+  const iv = crypto.randomBytes(12);
+
+  const key = crypto.pbkdf2Sync(secretKey, salt, iterations, keylen, digest);
+
+  const cipher = crypto.createCipheriv(algorithm, key, iv);
+
+  const enc1 = cipher.update(data, inputEncoding);
+
+  const enc2 = cipher.final();
+
+  const tag = cipher.getAuthTag();
+
+  const encryptedData = Buffer.concat([enc1, enc2, iv, tag]).toString(outputEncoding);
+
+  return encryptedData;
+};
+
+export const decrypt = (data, secretKey) => {
+  const inputEncoding = 'base64';
+  const outputEncoding = 'utf8';
+
+  const bufferData = Buffer.from(data, inputEncoding);
+
+  const key = crypto.pbkdf2Sync(secretKey, salt, iterations, keylen, digest);
+
+  const iv = bufferData.slice(bufferData.length - 28, bufferData.length - 16);
+
+  const tag = bufferData.slice(bufferData.length - 16);
+
+  const text = bufferData.slice(0, bufferData.length - 28);
+
+  const decipher = crypto.createDecipheriv(algorithm, key, iv);
+
+  decipher.setAuthTag(tag);
+
+  let str = decipher.update(text, undefined, outputEncoding);
+
+  str += decipher.final(outputEncoding);
+
+  return str;
+};
