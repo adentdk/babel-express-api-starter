2378667e1d2b964118b6af84febb8ee70b140410
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2022-07-14 13:19:26 +0700

    :sparkles: request parser middleware

---
+++ src/middlewares/requestParser.js
@@ -0,0 +1,38 @@
+import { StatusCodes } from 'http-status-codes';
+
+const requestParser = (keys = []) => (req, res, next) => {
+  const errorParse = [];
+  keys.forEach((key) => {
+    if (req.body[key]) {
+      try {
+        req.body[key] = JSON.parse(req.body[key]);
+      } catch (error) {
+        errorParse.push(key);
+      }
+    }
+
+    if (req.query[key]) {
+      try {
+        req.query[key] = JSON.parse(req.query[key]);
+      } catch (error) {
+        errorParse.push(key);
+      }
+    }
+  });
+
+  if (errorParse.length) {
+    const error = {
+      statusCode: StatusCodes.UNPROCESSABLE_ENTITY,
+      fields: errorParse.map((key) => ({
+        name: key,
+        message: 'Invalid value',
+      })),
+    };
+
+    return next(error);
+  }
+
+  return next();
+};
+
+export default requestParser;
