f530f49860f457e77e174469d07674ec52a27bb5
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2022-07-18 10:59:18 +0700

    remove unuse code

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,26 +4,8 @@ import config from '@config/config';
 import { Server } from 'socket.io';
 
 const onConnection = (io) => {
-  io.use((socket, next) => {
-    const { username } = socket.handshake.auth;
-
-    if (username) {
-      socket.username = username;
-      return next();
-    }
-
-    return next(new Error('must login'));
-  });
-
   io.on('connection', (socket) => {
     socket.emit('connected', true);
-    socket.on('message', (payload) => {
-      socket.emit('message', payload);
-      socket.broadcast.emit('message', payload);
-    });
-    socket.on('disconnect', async () => {
-
-    });
   });
 };
 
